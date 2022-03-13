228d73aa84b25cace3406a59498a1141117964d1
Yassine-cheffai <yacincheffai@yahoo.com>
2022-03-13 13:35:41 +0100

    solar system 2D

--- subjects/type_checking.py
+++ subjects/type_checking.py
@@ -7,4 +7,40 @@
 >>> names: List[str] = ["Guido", "Jukka", "Ivan"]
 >>> version: Tuple[int, int, int] = (3, 7, 1)
 >>> options: Dict[str, bool] = {"centered": False, "capitalize": True}
+
+python check types on runtime
+>>> if False:
+...     1 + "two"  # This line never runs, so no TypeError is raised
+... else:
+...     1 + 2
+...
+3
+
+so if there is a problem with the code, the exception won't be raised until that code execute, which is not a very good thing.import math
+
+>>>def circumference(radius: float) -> float:
+...    return 2 * math.pi * radius
+
+>>> circumference(1.23)
+7.728317927830891
+
+>>> circumference.__annotations__
+{'radius': <class 'float'>, 'return': <class 'float'>}
+
+>>> nothing: str
+>>> nothing
+NameError: name 'nothing' is not defined
+
+>>> __annotations__
+{'nothing': <class 'str'>}
+
+
+
+>>> from typing import List, Tuple
+>>> Card = Tuple[str, str]
+>>> Deck = List[Card]
+
+>>> Deck
+typing.List[typing.Tuple[str, str]]
+
 """
