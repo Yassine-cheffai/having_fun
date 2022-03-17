60444ef5c564a7e6049f16e6cba7f4c27ab084b6
Yassine-cheffai <yacincheffai@yahoo.com>
2022-03-17 09:31:46 +0100

    update

--- pynative/strings/string_solutions.py
+++ pynative/strings/string_solutions.py
@@ -1,10 +1,27 @@
-class Solution():
+class Solution:
     @staticmethod
     def problem_2():
         i = 1
         while i <= 5:
-            print(" ".join([str(j) for j in range(1, i+1)]))
+            print(" ".join([str(j) for j in range(1, i + 1)]))
             i += 1
 
+    @staticmethod
+    def problem_3(n: int):
+        sum = 0
+        for i in range(n + 1):
+            sum += i
+        return sum
 
-Solution.problem_2()
+    @staticmethod
+    def problem_4():
+        for i in range(1, 11):
+            print(i * 2)
+
+    @staticmethod
+    def problem_5(numbers: list):
+        for number in numbers:
+            if number > 500:
+                break
+            if number % 5 == 0 and number <= 150:
+                print(number)
--- pynative/strings/tests_string_solutions.py
+++ pynative/strings/tests_string_solutions.py
@@ -1,10 +1,36 @@
-import solution
+from string_solutions import Solution
 
-def test_problem(capsys):
-    solution.Solution.problem()
+
+def test_problem_2(capsys):
+    Solution.problem_2()
 
     captured = capsys.readouterr()
     out, _ = captured.out, captured.err
     expected_out = "1\n1 2\n1 2 3\n1 2 3 4\n1 2 3 4 5\n"
 
     assert out == expected_out
+
+
+def test_problem_3():
+    expected_result = 55
+    input = 10
+    assert Solution.problem_3(input) == expected_result
+
+
+def test_problem_4(capsys):
+    Solution.problem_4()
+    captured = capsys.readouterr()
+    out = captured.out
+    expected_result = "2\n4\n6\n8\n10\n12\n14\n16\n18\n20\n"
+
+    assert out == expected_result
+
+
+def test_problem_5(capsys):
+    numbers = [12, 75, 150, 180, 145, 525, 50]
+    Solution.problem_5(numbers)
+    captured = capsys.readouterr()
+    out = captured.out
+    expected_result = "75\n150\n145\n"
+
+    assert out == expected_result
