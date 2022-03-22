db13e7ac863586c1b900448dfcb2226c21962dba
Yassine-cheffai <yacincheffai@yahoo.com>
2022-03-22 11:04:11 +0100

    update

--- pynative/lists/lists_exercises.py
+++ pynative/lists/lists_exercises.py
@@ -23,3 +23,26 @@ class Solution:
                 result.append(f"{l1.strip()} {l2.strip()}")
 
         return result
+
+    @staticmethod
+    def exercise_5(list1: List[int], list2: List[int]):
+        for l1, l2 in zip(list1, reversed(list2)):
+            print(f"{l1} {l2}")
+
+    @staticmethod
+    def exercise_9(list1: List[int], number: int, new_number: int) -> List[int]:
+        try:
+            index = list1.index(number)
+            list1[index] = new_number
+            return list1
+        except ValueError:
+            return list1
+
+    @staticmethod
+    def exercise_10(list1: List[int], number_to_remove: int) -> List[int]:
+        # slow
+        # while number_to_remove in list1:
+        # list1.remove(number_to_remove)
+        # return list1
+
+        return [i for i in list1 if i != number_to_remove]
--- pynative/lists/tests_list_exercises.py
+++ pynative/lists/tests_list_exercises.py
@@ -28,3 +28,28 @@ def test_exercise_4():
     expected_result = ["Hello Dear", "Hello Sir", "take Dear", "take Sir"]
 
     assert Solution.exercise_4(list1, list2) == expected_result
+
+
+def test_exercise_5(capsys):
+    expected_result = "10 400\n20 300\n30 200\n40 100\n"
+    list1 = [10, 20, 30, 40]
+    list2 = [100, 200, 300, 400]
+    Solution.exercise_5(list1, list2)
+    captured = capsys.readouterr()
+    out = captured.out
+    assert out == expected_result
+
+
+def test_exercise_9():
+    list1 = [5, 10, 15, 20, 25, 50, 20]
+    number = 20
+    new_number = 200
+    expected_result = [5, 10, 15, 200, 25, 50, 20]
+    assert Solution.exercise_9(list1, number, new_number) == expected_result
+
+
+def test_exercise_10():
+    list1 = [5, 20, 15, 20, 25, 50, 20]
+    expected_result = [5, 15, 25, 50]
+    number_to_remove = 20
+    assert Solution.exercise_10(list1, number_to_remove) == expected_result
