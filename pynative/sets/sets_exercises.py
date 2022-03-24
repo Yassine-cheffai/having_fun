30e8fb7450bce51d8418357237b76114fea60454
Yassine-cheffai <yacincheffai@yahoo.com>
2022-03-24 01:59:01 +0100

    update

--- pynative/sets/sets_exercises.py
+++ pynative/sets/sets_exercises.py
@@ -6,3 +6,30 @@ class Solution:
     def exercise_1(sample_set: Set[str], sample_list: List[str]) -> Set[str]:
         sample_set.update(sample_list)
         return sample_set
+
+    @staticmethod
+    def exercise_2(set1: Set[int], set2: Set[int]) -> Set[int]:
+        return set1.intersection(set2)
+
+    @staticmethod
+    def exercise_3(set1: Set[int], set2: Set[int]) -> Set[int]:
+        return set1.union(set2)
+
+    @staticmethod
+    def exercise_4(set1: Set[int], set2: Set[int]) -> Set[int]:
+        return set1.difference(set2)
+
+    @staticmethod
+    def exercise_5(set1: Set[int], to_remove: Set[int]) -> Set[int]:
+        return set1.difference(to_remove)
+
+    @staticmethod
+    def exercise_6(set1: Set[int], set2: Set[int]) -> Set[int]:
+        # intersection = set1.intersection(set2)
+        # s = set()
+        # s.update(set1.difference(intersection))
+        # s.update(set2.difference(intersection))
+
+        # return s
+
+        return set1.symmetric_difference(set2)
--- pynative/sets/tests_sets_exercises.py
+++ pynative/sets/tests_sets_exercises.py
@@ -6,3 +6,43 @@ def test_exercise_1():
     sample_list = ["Blue", "Green", "Red"]
     expected_result = {"Green", "Yellow", "Black", "Orange", "Red", "Blue"}
     assert Solution.exercise_1(sample_set, sample_list) == expected_result
+
+
+def test_exercise_2():
+    set1 = {10, 20, 30, 40, 50}
+    set2 = {30, 40, 50, 60, 70}
+    expected_result = {40, 50, 30}
+
+    assert Solution.exercise_2(set1, set2) == expected_result
+
+
+def test_exercise_3():
+    set1 = {10, 20, 30, 40, 50}
+    set2 = {30, 40, 50, 60, 70}
+    expected_result = {70, 40, 10, 50, 20, 60, 30}
+
+    assert Solution.exercise_3(set1, set2) == expected_result
+
+
+def test_exercise_4():
+    set1 = {10, 20, 30}
+    set2 = {20, 40, 50}
+    expected_result = {10, 30}
+
+    assert Solution.exercise_4(set1, set2) == expected_result
+
+
+def test_exercise_5():
+    set1 = {10, 20, 30, 40, 50}
+    to_remove = {10, 20, 30}
+    expected_result = {40, 50}
+
+    assert Solution.exercise_5(set1, to_remove) == expected_result
+
+
+def test_exercise_6():
+    set1 = {10, 20, 30, 40, 50}
+    set2 = {30, 40, 50, 60, 70}
+    expected_result = {20, 70, 10, 60}
+
+    assert Solution.exercise_6(set1, set2) == expected_result
